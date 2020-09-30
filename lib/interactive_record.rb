d6b62fdd86ba365cc3c20a095fb13eac30664d8d
Andy Adams <andrewdcadams@gmail.com>
2020-09-30 00:27:08 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,8 +51,10 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
   
-  def self.find_by_name
-    
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{table_name_for_insert} WHERE name = #{name}"
+   
+    table_info = DB[:conn].execute(sql)
   end
   
   
